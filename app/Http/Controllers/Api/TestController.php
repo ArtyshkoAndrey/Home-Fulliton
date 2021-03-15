1507dd9f4f87c99cb6b5e1ea3fefd55a105a6331
Fulliton <artyshko.andrey@gmail.com>
2021-03-15 23:18:47 +0700

    Trait

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use App\Traits\GoogleAssitans;
 
 class TestController extends Controller
 {
+  use GoogleAssitans;
+
   public function test(Request $request)
   {
     return redirect($request->redirect_uri . '?code='. config('token.code') . '=' . $request->state);
@@ -18,9 +20,9 @@ class TestController extends Controller
   public function token(Request $request): JsonResponse
   {
     $data = [
-    "token_type" => "Bearer",
-    "access_token" => "1836.15267389",
-    "expires_in" => 36000
+      "token_type" => "Bearer",
+      "access_token" => "1836.15267389",
+      "expires_in" => 36000
     ];
 
     return response()->json($data);
@@ -28,60 +30,17 @@ class TestController extends Controller
 
   public function google_assistant (Request $request): JsonResponse
   {
-    $data = (object) [];
-    $data->requestId = $request->requestId;
-    $data->payload = [
-      "agentUserId" => "1836.15267389",
-      "devices" => [
-        [
-          "id" => "123",
-          "type" => "action.devices.types.OUTLET",
-          "traits" => ["action.devices.traits.OnOff"],
-          "name" => [
-            "defaultNames" => ["Свет на кухне"],
-            "name" => "Свет на кухне",
-            "nicknames" => ["Свет на кухне"],
-          ],
-          "willReportState" => false,
-          "roomHint" => "Кухня",
-          "deviceInfo" => [
-            "manufacturer" => "lights-out-inc",
-            "model" => "hs1234",
-            "hwVersion" => "3.2",
-            "swVersion" => "11.4"
-          ],
-          "attributes" => (object) [
-            "commandOnlyOnOff" => true,
-            "queryOnlyOnOff" => false
-          ]
-        ],
-        [
-          "id" => "1233",
-          "type" => "action.devices.types.THERMOSTAT",
-          "traits" => ["action.devices.traits.TemperatureControl"],
-          "name" => "Температура",
-          "willReportState" => true,
-          "roomHint" => "Кухня",
-          "deviceInfo" => [
-            "manufacturer" => "smart-home-inc",
-            "model" => "hs1234",
-            "hwVersion" => "3.2",
-            "swVersion" => "11.4"
-          ],
-          "attributes" => [
-            "temperatureRange" => [
-              "minThresholdCelsius" => 0,
-              "maxThresholdCelsius" => 35,
-              "temperatureAmbientCelsius" => 23.3
-            ],
-            "temperatureStepCelsius" => 1,
-            "temperatureUnitForUX" => "C",
-            "commandOnlyTemperatureControl" => true,
-            "queryOnlyTemperatureControl" => true,
-          ],
-        ],
-      ]
-    ];
+    $request->validate([
+      'inputs' => 'required|array',
+    ]);
+
+    if ($request->inputs[0]->intent === 'action.devices.SYNC') {
+      $data = $this->sync($request);
+    } else if ($request->inputs[0]->intent === 'action.devices.QUERY') {
+      $data = $this->query($request);
+    } else {
+      $data = []
+    }
 
     return response()->json($data);
   }
---
+++ app/Traits/GoogleAssitans.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+
+trait GoogleAssitans
+{
+
+  public function sync(Request $request): array
+  {
+    $data = [];
+    $data['requestId'] = $request->requestId;
+    $data['payload'] = [
+      "agentUserId" => "1836.15267389",
+      "devices" => [
+        [
+          "id" => "123",
+          "type" => "action.devices.types.OUTLET",
+          "traits" => ["action.devices.traits.OnOff"],
+          "name" => [
+            "defaultNames" => ["Свет на кухне"],
+            "name" => "Свет на кухне",
+            "nicknames" => ["Свет на кухне"],
+          ],
+          "willReportState" => false,
+          "roomHint" => "Кухня",
+          "deviceInfo" => [
+            "manufacturer" => "lights-out-inc",
+            "model" => "hs1234",
+            "hwVersion" => "3.2",
+            "swVersion" => "11.4"
+          ],
+          "attributes" => [
+            "commandOnlyOnOff" => true,
+            "queryOnlyOnOff" => false
+          ]
+        ],
+        [
+          "id" => "1233",
+          "type" => "action.devices.types.SENSOR",
+          "traits" => ["action.devices.traits.TemperatureControl"],
+          "name" => [
+            "defaultNames" => ["Температура на кухне"],
+            "name" => "Температура на кухне",
+            "nicknames" => ["Температура на кухне"],
+          ],
+          "willReportState" => false,
+          "roomHint" => "Кухня",
+          "deviceInfo" => [
+            "manufacturer" => "smart-home-inc",
+            "model" => "hs1234",
+            "hwVersion" => "3.2",
+            "swVersion" => "11.4"
+          ],
+          "attributes" => [
+            "temperatureRange" => [
+              "minThresholdCelsius" => 0,
+              "maxThresholdCelsius" => 35,
+              "temperatureAmbientCelsius" => 23
+            ],
+            "temperatureUnitForUX" => "C",
+            "commandOnlyTemperatureControl" => true,
+            "queryOnlyTemperatureControl" => true,
+          ],
+        ],
+      ]
+    ];
+
+    return $data;
+  }
+
+  public function query (Request $request)
+  {
+    $request->validate([
+      'inputs' => 'required|array',
+    ]);
+    $data = $request->all();
+    $input = $data['inputs'][0];
+    if ($input['intent'] === 'action.devices.QUERY') {
+      $deviceId = $input['payload']['devices'][0]['id'];
+      if ($deviceId === '1233') {
+        return $this->getTemperatureControl($data['requestId']);
+      }
+    }
+    return [];
+  }
+
+  private function getTemperatureControl ($requestId): array
+  {
+    return [
+      "requestId" => $requestId,
+      "payload" => [
+        "devices" => [
+          "1233" => [
+            "status" => "SUCCESS",
+            "online" => true,
+            "temperatureRange" => [
+              "temperatureAmbientCelsius" => 32
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
+
+}
